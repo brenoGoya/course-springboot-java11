3ed88f29a94ff467ae0892d0cc581e05244ebe6f
Breno Goya <breno.goya@gmail.com>
2021-02-08 11:20:11 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/neliocourse/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.neliocourse.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.neliocourse.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+}
