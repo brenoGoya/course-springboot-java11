56d3acbd3fd17f1ea09531434ae1e9fb54b7552f
Breno Goya <breno.goya@gmail.com>
2021-02-08 13:58:23 -0300

    Service layer, component registration

---
+++ src/main/java/com/neliocourse/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.neliocourse.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.neliocourse.course.entities.User;
+import com.neliocourse.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired // framework dependency injection
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
